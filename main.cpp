e65d8bcd42bb4ce68c40ad8302e812eac4e2e5ff
sccluisx <sccluisx@gmail.com>
2023-01-27 02:49:32 +0100

    test1

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+git config --global user.email "sccluisx@gmail.com" git config --global user.name "sccluisx"
